a5b8bb3245efcd5a0c537d22b01bbe72adc958f0
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2023-08-06 09:21:35 +0100

    Graphic Text and Code Upgrade.

--- AutoClix.bat
+++ AutoClix.bat
@@ -1,11 +1,27 @@
-echo Time to click in style..!
-timeout /t 2 >nul
+@echo off
 echo.
 echo.
 
+
+:: Graphic Text
+echo "   _____          __         _________ .__  .__        "
+echo "  /  _  \  __ ___/  |_  ____ \_   ___ \|  | |__|__  ___"
+echo " /  /_\  \|  |  \   __\/  _ \/    \  \/|  | |  \  \/  /"
+echo "/    |    \  |  /|  | (  <_> )     \___|  |_|  |>    < "
+echo "\____|__  /____/ |__|  \____/ \______  /____/__/__/\_ \"
+echo "        \/                           \/              \/"
+echo.
+echo.
+timeout /t 2 >nul 
+echo.
+echo.
+
+:: Run Program.
+@echo on
 python.exe autoclix.py
 echo.
 echo.
 
+:: Program Exit
 timeout /t 2 >nul  
 pause
